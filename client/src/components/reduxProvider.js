6e317e4117dda79e2dbec4ab82d0c553dd845669
Evan <evan@crowdhubapps.com>
2017-07-20 17:59:13 -0700

    redux offline, redux provider, unhardcode things in ocaml

---
+++ client/src/components/reduxProvider.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import Thunk from "redux-thunk";
+import { offline } from "redux-offline";
+import offlineConfig from "redux-offline/lib/defaults";
+import BoardList from "../reducers/boardList";
+import ThreadList from "../reducers/threadList";
+import PostList from "../reducers/postList";
+import Settings from "../reducers/settings";
+
+const reducers = combineReducers({ BoardList, ThreadList, PostList, Settings });
+const middlewares = compose(applyMiddleware(Thunk), offline(offlineConfig));
+const store = createStore(reducers, middlewares);
+
+export default function({ children }) {
+  return <Provider store={store} children={children} />;
+}
--- client/src/main.js
+++ client/src/main.js
@@ -1,43 +1,27 @@
 import React from "react";
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import { Provider } from "react-redux";
-import Thunk from "redux-thunk";
 import { StackNavigator } from "react-navigation";
 import { ActionSheetProvider } from "@expo/react-native-action-sheet";
+import ReduxProvider from "./components/reduxProvider";
 import StyleProvider, { cardStyle } from "./components/styleProvider";
-import BoardList from "./reducers/boardList";
-import ThreadList from "./reducers/threadList";
-import PostList from "./reducers/postList";
-import Settings from "./reducers/settings";
 import Home from "./scenes/home";
 import Board from "./scenes/board";
 import Thread from "./scenes/thread";
 import Reply from "./scenes/reply";
-import SettingsScene from "./scenes/settings";
+import Settings from "./scenes/settings";
 
-const reducers = combineReducers({ BoardList, ThreadList, PostList, Settings });
-const store = createStore(reducers, applyMiddleware(Thunk));
 const Stacks = StackNavigator(
-  {
-    Home,
-    Board,
-    Thread,
-    Settings: SettingsScene,
-    Reply
-  },
-  {
-    cardStyle
-  }
+  { Home, Board, Thread, Settings, Reply },
+  { cardStyle }
 );
 
 export default function() {
   return (
-    <Provider store={store}>
+    <ReduxProvider>
       <StyleProvider>
         <ActionSheetProvider>
           <Stacks onNavigationStateChange={null} />
         </ActionSheetProvider>
       </StyleProvider>
-    </Provider>
+    </ReduxProvider>
   );
 }
