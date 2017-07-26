f4da3191951bf36a66215c04d65a36fdfe8aef4f
Evan <evan@crowdhubapps.com>
2017-07-25 23:33:38 -0700

    fixing persisting local storage image bug in thread list

--- client/src/components/appProvider.js
+++ client/src/components/appProvider.js
@@ -1,6 +1,7 @@
 import { connect } from "react-redux";
 import { lifecycle } from "recompose";
 import { requestToken } from "../reducers/app";
+import { clearThreadList } from "../reducers/threadList";
 
 function AppContainer({ children }) {
   return children;
@@ -8,11 +9,15 @@ function AppContainer({ children }) {
 
 function componentWillMount() {
   // this.props.requestToken();
+  // Persisting this will mess
+  // with images.
+  this.props.clearThreadList();
 }
 
 function mapDispatch(dispatch) {
   return {
-    requestToken: () => dispatch(requestToken())
+    requestToken: () => dispatch(requestToken()),
+    clearThreadList: () => dispatch(clearThreadList())
   };
 }
 
