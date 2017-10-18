dad01c90368a6cfe083c1070a3d95f74aa0dbd95
minieggs <evan@crowdhubapps.com>
2017-10-17 21:40:12 -0700

    Adding server side check for creating too many suggestions.

--- server/src/routes/CreateSuggestion.js
+++ server/src/routes/CreateSuggestion.js
@@ -1,16 +1,38 @@
+import Sequelize from "sequelize";
 import { Suggestions } from "../Models";
 
 const MSG_COMPLETE = "Suggestion has been submitted. Thank you.";
 const MSG_ERROR = "Could not complete this request at this time.";
+const FIFTEEN_MINUTES = 1000 * 60 * 15;
+
+const getCount = deviceID =>
+  Suggestions.findAll({
+    where: {
+      deviceID,
+      createdAt: {
+        [Sequalize.Op.gt]: new Date(new Date() - FIFTEEN_MINUTES)
+      }
+    }
+  });
 
 export default async (req, res) => {
   const { deviceID, content } = req.body;
 
   try {
-    if (!deviceID || !content) throw new Error("Parameters not set.");
+    // Check params.
+    if (!deviceID || !content) throw new Error("Missing required parameters.");
+
+    // Make sure user isn't doing this too often.
+    const count = await getCount(deviceID);
+    if (count > 1) throw new Error("Your doing that too often.");
+
+    // Create it!
     await Suggestions.create({ deviceID, content });
     res.send({ message: MSG_COMPLETE, status: true });
   } catch (err) {
-    res.send({ message: MSG_ERROR, status: false });
+    // Show the error if clients can display it.
+    // Default the message otherwise.
+    const message = typeof err === "string" ? err : MSG_ERROR;
+    res.send({ message, status: false });
   }
 };
